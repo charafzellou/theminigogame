0d783960c07fabf62ee7563f72547bb18813f1a6
Charaf ZELLOU <charaf.zellou@gmail.com>
2019-07-03 14:15:01 +0200

    Added classes, damages
    Added some comments, added all the structs necessary for the battle system, Main function allows to try damage (not via attacks).

--- main_rpg.go
+++ main_rpg.go
@@ -1,26 +1,25 @@
 package main
 
+// Importing necessary dependacies
 import (
 	"fmt"
 )
 
-type assetFighter struct {
+// Initializing assets and events Structures
+type assetPlayer struct {
 	name  string
 	class assetClass
 }
-
 type assetClass struct {
 	name     string
 	health   uint
 	strength uint
 }
-
 type eventAttack struct {
 	name   string
 	class  assetClass
 	damage uint
 }
-
 type eventCombo struct {
 	name        string
 	class       assetClass
@@ -29,25 +28,51 @@ type eventCombo struct {
 	damageBonus uint
 }
 
-func hit(target *assetFighter, strength uint) {
-	target.class.health -= strength
+// Initializing in-game assets
+func initClasses() {
+}
+func initAttacks() {
+}
+func initCombos() {
+}
+func initPlayers() {
 }
 
-func getHit() {
-
+// Initializing in-game functions
+func hit(attacker assetPlayer, target assetPlayer) {
+	fmt.Println(target.name, "had", target.class.health , "HP and took" , attacker.class.strength, "damage from", attacker.name)
+	target.class.health -= attacker.class.strength
+	fmt.Println(target.name, "has currently", target.class.health, "HP!")
+}
+func getHit(target assetPlayer, amount uint){
+	target.class.health -= amount
+}
+func heal(healer assetPlayer, target assetPlayer) {
+	target.class.health += healer.class.strength
+}
+func getHealed(target assetPlayer, amount uint){
+	target.class.health += amount
 }
 
+// Initializing Main Storyline
 func main() {
 	paladin := assetClass{"Paladin", 100, 20}
+	archer := assetClass{"Archer", 75, 25}
 	ninja := assetClass{"Ninja", 80, 35}
 
-	fighterJuan := assetFighter{"Juan", paladin}
+	paladinJuan := assetPlayer{"Juanitus", paladin}
+	archerJuan := assetPlayer{"Juanito", archer}
+	ninjaJuan := assetPlayer{"Juan", ninja}
 
-	fmt.Println(paladin)
-	fmt.Println(ninja)
-	fmt.Println(fighterJuan)
+	/*fmt.Println(paladinJuan.class.health)
+	fmt.Println(archerJuan.class.health)
+	fmt.Println(ninjaJuan.class.health)*/
 
-	hit(&fighterJuan, fighterJuan.class.strength)
+	hit(paladinJuan, archerJuan)
+	hit(ninjaJuan, paladinJuan)
+	hit(archerJuan, ninjaJuan)
 
-	fmt.Println(fighterJuan)
+	/*fmt.Println(paladinJuan.class.health)
+	fmt.Println(archerJuan.class.health)
+	fmt.Println(ninjaJuan.class.health)*/
 }
