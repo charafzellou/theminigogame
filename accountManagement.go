7b06f3551144930e78dea366108499152eeb0ce8
Juan Jose <juan.urrego@finelia.fr>
2019-07-25 19:22:29 +0200

    added getAccounts and saveAccount
    functions that retrieve and save  accounts in .data directory

--- accountManagement.go
+++ accountManagement.go
@@ -19,3 +19,28 @@ type assetAccount struct {
 	AccountPwd    [32]byte
 	Player        assetPlayer
 }
+// not pure att all
+func getAccounts(){
+	content, err := ioutil.ReadFile(".data/accounts.secure")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(content, &accountsList)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+// not pure either
+func saveAccounts(){
+	jsonAccounts, err := json.Marshal(accountsList)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(".data/accounts.secure", jsonAccounts, 0644)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Account successfully saved")
+	}
+}
